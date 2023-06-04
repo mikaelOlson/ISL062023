f5adff5972911558debd1dc06c4000e72b934138
Mikael <mikaelolson94@gmail.com>
2023-06-04 19:54:54 +0200

    commit to work on laptop - authentication working

--- src/test/java/com/infrasight/kodtest/Tests.java
+++ src/test/java/com/infrasight/kodtest/Tests.java
@@ -3,122 +3,160 @@ package com.infrasight.kodtest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.infrasight.kodtest.api.auth.AuthRequest;
 import okhttp3.*;
 import okhttp3.internal.http2.Header;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.json.*;
+import javax.json.Json.*;
 
 /**
  * Simple concrete class for JUnit tests with uses {@link TestsSetup} as a
  * foundation for starting/stopping the API server for tests.
- * 
+ * <p>
  * You may configure port, api user and api port in {@link TestVariables} if
  * needed.
  */
 public class Tests extends TestsSetup {
 
-	/**
-	 * Simple example test which asserts that the Kodtest API is up and running.
-	 */
-	@Test
-	public void connectionTest() throws InterruptedException {
-		assertTrue(serverUp);
-	}
+    /**
+     * Simple example test which asserts that the Kodtest API is up and running.
+     */
+    @Test
+    public void connectionTest() throws InterruptedException {
+        assertTrue(serverUp);
+    }
 
-	@Test
-	public void assignment1() throws InterruptedException, IOException {
-		assertTrue(serverUp);
+    @Test
+    public void assignment1() throws InterruptedException, IOException {
+        assertTrue(serverUp);
 
-		/**
-		 * TODO: Add code to solve the first assignment. Add Assert to show that you
-		 * found the account for Vera
-		 */
+        /**
+         * TODO: Add code to solve the first assignment. Add Assert to show that you
+         * found the account for Vera
+         */
 
-		//Authenticate user and get token
+        //Authenticate user and get token
+        String authEndpoint = "/api/auth";
 
-		String authEndpoint = "/api/auth";
-
-		//OkHttpClient client = new OkHttpClient();
-
-		/*FormBody authRequestBody = new FormBody.Builder()
-				.add("user", TestVariables.API_USER)
-				.add("password", TestVariables.API_PASSWORD)
-				.build();*/
-
-		//TODO: CREATE JSON WITH USER AND PASSWORD
-		Gson gson = new Gson();
-		String requestBodyString = gson.toJson(authRequestBody);
-
-		RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), requestBodyString);
+        Map<String, String> creds = new HashMap<>();
+        creds.put("password", "apiPassword");
+        creds.put("user", "apiUser");
 
 
+        ObjectMapper mapper = new ObjectMapper();
 
-		Request authRequest = new Request.Builder()
-				.url(TestVariables.API_URL + TestVariables.API_PORT + authEndpoint)
-				.post(requestBody)
-				.build();
+        String json = mapper.writeValueAsString(creds);
 
-		OkHttpClient client = getHttpClientBuilder().build();
+        System.out.println(json);
 
-		Call call = client.newCall(authRequest);
-
-		Response response = call.execute();
-
-		assertTrue(response.code()==200 && !response.body().string().isEmpty());
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), json);
 
 
-	}
+        Request authRequest = new Request.Builder()
+                .url(TestVariables.API_URL + TestVariables.API_PORT + authEndpoint)
+                .post(requestBody)
+                .build();
 
-	@Test
-	public void assignment2() throws InterruptedException {
-		assertTrue(serverUp);
 
-		/**
-		 * TODO: Add code to solve the second assignment where we expect the number of
-		 * groups to be 3.
-		 */
-		int groupCount = 0;
+        //create client
+        OkHttpClient client = getHttpClientBuilder().build();
 
-		// Assert which verifies the expected group count of 3
-		assertEquals(3, groupCount);
+        Call call = client.newCall(authRequest);
 
-		/**
-		 * TODO: Add Assert to verify the IDs of the groups found
-		 */
-	}
+        Response response = call.execute();
 
-	@Test
-	public void assignment3() throws InterruptedException {
-		assertTrue(serverUp);
+        ObjectMapper mapperResponse = new ObjectMapper();
+        String tokenString = mapperResponse.writeValueAsString(response.body().string());
 
-		/**
-		 * TODO: Add code to solve the third assignment. Add Assert to verify the
-		 * expected number of groups. Add Assert to verify the IDs of the groups found.
-		 */
-	}
+        String token = tokenString.split(":")[1].split("\"")[1].replace("\\", "");
 
-	@Test
-	public void assignment4() throws InterruptedException {
-		assertTrue(serverUp);
 
-		/**
-		 * TODO: Add code to solve the fourth assignment. Add Asserts to verify the
-		 * total salary requested
-		 */
-	}
+        System.out.println("Token: " + token);
 
-	@Test
-	public void assignment5() throws InterruptedException {
-		assertTrue(serverUp);
 
-		/**
-		 * TODO: Add code to solve the fifth assignment. Add Asserts to verify the
-		 * managers requested
-		 */
-	}
+        //code to get all Vera's accounts
+
+        String accountsEndpoint = "/api/accounts";
+        String url = TestVariables.API_URL+TestVariables.API_PORT+accountsEndpoint;
+
+
+
+        Request accountRequest = new Request.Builder()
+                .header("Authorization", "Bearer " + token)
+                .url(url)
+                .get()
+                .build();
+
+        Call call2 = client.newCall(accountRequest);
+
+        Response response2 = call2.execute();
+
+
+        assertTrue(response.code() == 200);
+
+
+    }
+
+    @Test
+    public void assignment2() throws InterruptedException {
+        assertTrue(serverUp);
+
+        /**
+         * TODO: Add code to solve the second assignment where we expect the number of
+         * groups to be 3.
+         */
+        int groupCount = 0;
+
+        // Assert which verifies the expected group count of 3
+        assertEquals(3, groupCount);
+
+        /**
+         * TODO: Add Assert to verify the IDs of the groups found
+         */
+    }
+
+    @Test
+    public void assignment3() throws InterruptedException {
+        assertTrue(serverUp);
+
+        /**
+         * TODO: Add code to solve the third assignment. Add Assert to verify the
+         * expected number of groups. Add Assert to verify the IDs of the groups found.
+         */
+    }
+
+    @Test
+    public void assignment4() throws InterruptedException {
+        assertTrue(serverUp);
+
+        /**
+         * TODO: Add code to solve the fourth assignment. Add Asserts to verify the
+         * total salary requested
+         */
+    }
+
+    @Test
+    public void assignment5() throws InterruptedException {
+        assertTrue(serverUp);
+
+        /**
+         * TODO: Add code to solve the fifth assignment. Add Asserts to verify the
+         * managers requested
+         */
+    }
 }
